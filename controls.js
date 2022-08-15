d9d7789eecc7b81293621c9f108baaff256c00b0
Luis Sergio <tectualonline@gmail.com>
2022-08-15 12:06:15 -0300

    Commit inicial

---
+++ controls.js
@@ -0,0 +1,47 @@
+export default function Controls({
+    buttonPause,
+    buttonPlay,
+    buttonStop
+  }) {
+  
+    function play() {
+      buttonPlay.classList.add('hide')
+      buttonPause.classList.remove('hide')
+      buttonStop.classList.remove('hide')
+    }
+  
+    function pause() {
+      buttonPause.classList.add('hide')
+      buttonPlay.classList.remove('hide')
+    }
+    
+    function reset() {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonStop.classList.add('hide')
+    }
+
+    function add() {
+      addminutes
+    }
+
+    function remove() {
+      removeminutes
+    }
+  
+    function getMinutes() {
+      let newMinutes = prompt('Quantos minutos?')
+      if (!newMinutes) {
+        return false
+      }
+  
+      return newMinutes
+    }
+  
+    return {
+      reset,
+      play,
+      pause,
+      getMinutes
+    }
+  }
---
+++ index.js
@@ -0,0 +1,72 @@
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import {    
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay,
+  addMinutes,
+  removeMinutes,
+ } from "./elements.js"
+ import Sound from "./sounds.js"
+
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonStop,
+  addMinutes,
+  removeMinutes,
+})
+
+const timer = Timer({
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset
+})
+
+const sound = Sound()
+
+
+buttonPlay.addEventListener('click', function() {
+  controls.play()
+  timer.countdown()
+  sound.pressButton()
+})
+
+buttonPause.addEventListener('click', function() {
+  controls.pause()
+  timer.hold()
+  sound.pressButton()
+})
+
+buttonStop.addEventListener('click', function() {
+  controls.reset()
+  timer.reset()
+  sound.pressButton()
+})
+
+addMinutes.addEventListener('click', function() {
+
+  // let newMinutes = controls.getMinutes()
+
+  // if (!newMinutes) {
+  //   timer.reset()
+  //   return
+  // }
+  let minutes = parseInt(String(minutesDisplay.textContent));
+
+  timer.updateMinutes(minutes + 5);
+  timer.updateDisplay(minutes + 5, 0);
+  timer.updateMinutes(minutes - 5)
+  timer.updateDisplay(minutes - 5, 0);
+
+})
+
+
+
+
+
+
+
